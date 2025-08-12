d90134c7a7b39f48ca280a99eb266fa875c84303
mikeartee <mikeartee@gmail.com>
2025-08-13 04:50:10 +1200

    Add Tauri setup and Windows app launcher

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,21 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+use tauri::Manager;
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![])
+        .setup(|app| {
+            #[cfg(debug_assertions)]
+            {
+                let window = app.get_window("main").unwrap();
+                window.open_devtools();
+            }
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
